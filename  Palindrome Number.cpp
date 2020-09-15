dc47fe52832ee0718f1d1ec64817780b711af0f1
VladaChernoray <57546413+VladaChernoray@users.noreply.github.com>
2020-09-15 22:47:44 +0300

    Add files via upload

---
+++  Palindrome Number.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    bool isPalindrome(int x) {
+        int y = x;
+        if (x < 0) return false;
+        long long newX = 0;
+        if (x) {
+            while (x) {
+                newX = newX * 10 + x % 10;
+                x = x / 10;
+            }
+        }
+        if (y == newX) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
